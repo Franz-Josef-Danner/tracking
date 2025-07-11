5aef63d380b68a86d9efd85778b2da98af955248
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-11 14:42:35 +0200

    Fix import path after module rename

--- detect.py
+++ detect.py
@@ -1,6 +1,6 @@
 import bpy
 from margin_a_distanz import compute_margin_distance
-# ``ensure_margin_distance`` moved to ``margin_distance_adapt`` after module rename
+# ``ensure_margin_distance`` now lives in ``margin_distance_adapt``
 from margin_distance_adapt import ensure_margin_distance
 from adjust_marker_count_plus import adjust_marker_count_plus
 from count_new_markers import count_new_markers
