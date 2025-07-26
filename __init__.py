91eb044826b287e1d05c01abb2a35e8f63baddb2
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-26 19:27:03 +0200

    Reset global lists on register

--- __init__.py
+++ __init__.py
@@ -51,6 +51,10 @@ def register():
     for cls in classes:
         bpy.utils.register_class(cls)
 
+    # Reset global state lists in case of leftover data
+    functions.PENDING_RENAME = []
+    functions.NF = []
+
 
 def unregister():
     for cls in reversed(classes):
