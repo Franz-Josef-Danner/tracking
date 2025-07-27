2f6a186f736d61401893ab0be713831a5860ba08
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-27 17:58:39 +0200

    Delete ui/panels/__init__.py

--- ui/panels/__init__.py
+++
@@ -1,8 +0,0 @@
-# Absolute imports for panel modules
-from t.ui.panels import tracking_panel, settings_panel, test_panels
-
-panel_classes = (
-    *tracking_panel.panel_classes,
-    *settings_panel.panel_classes,
-    *test_panels.panel_classes,
-)
