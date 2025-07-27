892f41733e8a68a1742cdefb460bc7e1e8027587
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-27 18:13:21 +0200

    Update __init__.py

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,4 +1,4 @@
 # Absolute import to the tracking operator package
-from tracking-main.operators import tracking
+from tracking_main.operators import tracking
 
 operator_classes = tracking.operator_classes
