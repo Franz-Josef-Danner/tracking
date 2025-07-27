0fe9ac7447896f658e2d7270e13a7fdd1ed03379
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-27 18:13:10 +0200

    Update __init__.py

--- ui/panels/__init__.py
+++ ui/panels/__init__.py
@@ -1,5 +1,5 @@
 # Absolute imports for panel modules
-from tracking-main.ui.panels import tracking_panel, settings_panel, test_panels
+from tracking_main.ui.panels import tracking_panel, settings_panel, test_panels
 
 panel_classes = (
     *tracking_panel.panel_classes,
