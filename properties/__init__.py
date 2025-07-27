5bf77f9e1e2de2ad99f89e36a806ec4d5ba4ce81
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-27 17:48:14 +0200

    Create __init__.py

---
+++ properties/__init__.py
@@ -0,0 +1,12 @@
+# Absolute imports for property modules
+from t.properties import tracking_props, test_props
+
+
+def register_properties():
+    tracking_props.register_props()
+    test_props.register_props()
+
+
+def unregister_properties():
+    test_props.unregister_props()
+    tracking_props.unregister_props()
