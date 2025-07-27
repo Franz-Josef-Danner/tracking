c9b4417479c14024a39a53a2f1693e4cc81ce090
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-27 17:38:17 +0200

    neu
    besser

--- operators/tracking/__init__.py
+++
@@ -1,8 +0,0 @@
-# Import operator modules via absolute paths
-from t.operators.tracking import solver, camera, export
-
-operator_classes = (
-    *solver.operator_classes,
-    *camera.operator_classes,
-    *export.operator_classes,
-)
