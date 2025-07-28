0b6255e120eda46991fcd7d9465f7a800901c230
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-28 19:42:41 +0200

    Centralize marker target calculations

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -3,4 +3,6 @@ from .utils import *
 from .feature_math import (
     calculate_base_values,
     apply_threshold_to_margin_and_distance,
+    marker_target_aggressive,
+    marker_target_conservative,
 )
--- helpers/test_feature_math.py
+++ helpers/test_feature_math.py
@@ -1,6 +1,8 @@
 from .feature_math import (
     calculate_base_values,
     apply_threshold_to_margin_and_distance,
+    marker_target_aggressive,
+    marker_target_conservative,
 )
 
 
@@ -14,3 +16,12 @@ def test_threshold_scaling():
     margin, distance = apply_threshold_to_margin_and_distance(0.5, 100, 200)
     assert margin == 50
     assert distance == 100
+
+
+def test_marker_aggressive():
+    assert marker_target_aggressive(100) == 400
+
+
+def test_marker_conservative():
+    assert marker_target_conservative(90) == 30
+    assert marker_target_conservative(2) == 1
