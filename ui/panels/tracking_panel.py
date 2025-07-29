98ba4a0b1f693abcd8d11713714cd38094649753
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-29 12:11:26 +0200

    Show tracking steps in stufen panel

--- ui/panels/tracking_panel.py
+++ ui/panels/tracking_panel.py
@@ -1,7 +1,5 @@
 import bpy
 
-from .settings_panel import get_tracking_steps_info
-
 class CLIP_PT_tracking_panel(bpy.types.Panel):
     bl_space_type = 'CLIP_EDITOR'
     bl_region_type = 'UI'
@@ -13,24 +11,4 @@ class CLIP_PT_tracking_panel(bpy.types.Panel):
         layout.label(text="Addon Informationen")
 
 
-class CLIP_PT_tracking_steps(bpy.types.Panel):
-    bl_space_type = 'CLIP_EDITOR'
-    bl_region_type = 'UI'
-    bl_category = 'Track'
-    bl_label = 'Tracking Schritte'
-
-    def draw(self, context):
-        layout = self.layout
-
-        layout.operator('clip.track_nr1', text='Track Nr. 1')
-
-        layout.label(text="Automatischer Ablauf:")
-        box = layout.box()
-        for name, desc in get_tracking_steps_info():
-            box.label(text=f"{name} \u2013 {desc}")
-
-
-panel_classes = (
-    CLIP_PT_tracking_panel,
-    CLIP_PT_tracking_steps,
-)
+panel_classes = (CLIP_PT_tracking_panel,)
