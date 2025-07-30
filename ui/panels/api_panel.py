a248753acc389b49fb8f9c22d7e48dffe1bf61a5
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-30 03:58:25 +0200

    Add marker basis value operator

---
+++ ui/panels/api_panel.py
@@ -0,0 +1,19 @@
+import bpy
+
+
+class TRACKING_PT_api_functions(bpy.types.Panel):
+    bl_label = "API Funktionen"
+    bl_idname = "TRACKING_PT_api_functions"
+    bl_space_type = 'CLIP_EDITOR'
+    bl_region_type = 'UI'
+    bl_category = "Tracking Tools"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(context.scene, 'marker_basis', text='Marker/Frame')
+        layout.operator('tracking.marker_basis_values')
+
+
+panel_classes = (
+    TRACKING_PT_api_functions,
+)
