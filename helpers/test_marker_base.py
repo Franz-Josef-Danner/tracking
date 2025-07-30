2fffdba9bd4311f21651b1a02065d0966398a251
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-30 11:30:08 +0200

    Add helper function for marker base calculations

---
+++ helpers/test_marker_base.py
@@ -0,0 +1,21 @@
+import bpy
+
+
+def test_marker_base(context):
+    """Berechnet Marker-Basiswerte aus dem Eingabefeld 'marker/Frame'."""
+
+    scene = context.scene
+    marker_basis = scene.get("marker_per_frame", 12)
+
+    marker_plus = marker_basis / 3
+    marker_adapt = marker_plus
+    max_marker = marker_adapt * 1.1
+    min_marker = marker_adapt * 0.9
+
+    return {
+        "marker_basis": marker_basis,
+        "marker_plus": marker_plus,
+        "marker_adapt": marker_adapt,
+        "max_marker": max_marker,
+        "min_marker": min_marker,
+    }
