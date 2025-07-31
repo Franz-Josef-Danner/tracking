52634f7f229d9bee3433f8c3117646600dbe186f
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-07-31 19:36:37 +0200

    Create __init__.py

---
+++ ui/panels/__init__.py
@@ -0,0 +1,11 @@
+from .api_panel import TRACKING_PT_api_functions
+from .test_panel import (
+    TRACKING_PT_test,
+    TRACKING_PT_test_details,
+)
+
+panel_classes = (
+    TRACKING_PT_api_functions,
+    TRACKING_PT_test,
+    TRACKING_PT_test_details,
+)
