75daf43e488392976247171ffb68b60aae3083f3
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-01 19:22:05 +0200

    Update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -6,5 +6,12 @@ from .cycle_motion_model import cycle_motion_model
 from .set_tracking_channels import set_tracking_channels, CLIP_OT_set_tracking_channels
 from .proxy_enable import enable_proxy
 from .proxy_disable import disable_proxy
+from .test_cyclus import (
+    evaluate_tracking,
+    find_optimal_pattern,
+    find_optimal_motion,
+    find_best_channel_combination,
+    run_tracking_optimization,
+)
 from .low_marker_frame import low_marker_frame
 from .invoke_clip_operator_safely import invoke_clip_operator_safely
