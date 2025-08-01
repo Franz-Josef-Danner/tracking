4b13ea6bdaa09266d0b2c86ab3b175782e1f90bb
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-01 06:44:31 +0200

    Update __init__.py

--- properties/__init__.py
+++ properties/__init__.py
@@ -1,8 +1,7 @@
-from .tracking_props import register_props, unregister_props
+from . import tracking_props
 
 def register_properties():
     tracking_props.register_props()
-
 
 def unregister_properties():
     tracking_props.unregister_props()
