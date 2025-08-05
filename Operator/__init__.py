f06d959144c28e00cd5d6e7c89c8a6e566658f0e
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-05 04:33:35 +0200

    Update __init__.py

--- Operator/__init__.py
+++ Operator/__init__.py
@@ -1,3 +1,5 @@
+import bpy
+
 from .proxy_builder import CLIP_OT_proxy_builder
 from .tracker_settings import CLIP_OT_tracker_settings
 from .detect import CLIP_OT_detect
@@ -9,3 +11,13 @@ operator_classes = (
     CLIP_OT_detect,
     CLIP_OT_bidirectional_track,
 )
+
+def register():
+    for cls in operator_classes:
+        bpy.utils.register_class(cls)
+
+def unregister():
+    for cls in reversed(operator_classes):
+        bpy.utils.unregister_class(cls)
+if __name__ == "__main__":
+    register()
