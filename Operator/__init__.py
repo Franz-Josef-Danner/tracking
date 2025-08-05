6a6bc14621715c0b88e4413e437eae03b05cf64a
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-05 03:45:21 +0200

    Update __init__.py

--- Operator/__init__.py
+++ Operator/__init__.py
@@ -1,9 +1,11 @@
 from .proxy_builder import CLIP_OT_proxy_builder
 from .tracker_settings import CLIP_OT_tracker_settings
 from .detect import CLIP_OT_detect
+from .bidirectional_track import CLIP_OT_bidirectional_track
 
 operator_classes = (
     CLIP_OT_proxy_builder,
     CLIP_OT_tracker_settings,
     CLIP_OT_detect,
+    bidirectional_track,
 )
