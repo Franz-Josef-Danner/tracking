126999788e2b2eebccb1684708225fde2002df6b
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-05 12:38:35 +0200

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -2,10 +2,12 @@ from .marker_helper_main import CLIP_OT_marker_helper_main
 from .disable_proxy import CLIP_OT_disable_proxy
 from .enable_proxy import CLIP_OT_enable_proxy
 from .select_short_tracks import select_short_tracks
+from .delete_selected_markers import delete_selected_markers
 
 operator_classes = (
     marker_helper_main,
     enable_proxy,
     disable_proxy,
     select_short_tracks,
+    delete_selected_markers,
 )
