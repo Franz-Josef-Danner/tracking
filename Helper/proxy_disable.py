eccd78a0fd078095e2d1af9e545ee96706ad044d
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-05 03:16:12 +0200

    Update proxy_disable.py

--- Helper/proxy_disable.py
+++ Helper/proxy_disable.py
@@ -1,5 +1,9 @@
 import bpy
 
+class CLIP_OT_proxy_disable(bpy.types.Operator):
+    bl_idname = "clip.proxy_disable"
+    bl_label = "Proxy Deaktivieren"
+
 def disable_proxy(context):
     clip = getattr(context.space_data, "clip", None)
     if not clip:
