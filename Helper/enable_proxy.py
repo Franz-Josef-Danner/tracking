b0ba82a374b31851fe0bef2a9681d91dad00289f
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-05 03:09:21 +0200

    Create enable_proxy.py

---
+++ Helper/enable_proxy.py
@@ -0,0 +1,15 @@
+import bpy
+
+def enable_proxy(context):
+    clip = getattr(context.space_data, "clip", None)
+    if not clip:
+        print("Kein gültiger Movie Clip gefunden.")
+        return
+
+    clip.use_proxy = True
+    for proxy in clip.proxy.proxy_storage:
+        proxy.build_25 = True
+        proxy.build_50 = True
+        proxy.build_75 = True
+        proxy.build_100 = True
+    print("Proxy aktiviert.")
