f5294104140edb84ae4c20165593fc50aa84cd6a
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-05 03:10:54 +0200

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -1,5 +1,9 @@
 from .marker_helper_main import CLIP_OT_marker_helper_main
+from .proxy_disable import CLIP_OT_proxy_disable
+from .enable_proxy import CLIP_OT_enable_proxy
 
 operator_classes = (
     marker_helper_main,
+    proxy_disable,
+    enable_proxy,
 )
