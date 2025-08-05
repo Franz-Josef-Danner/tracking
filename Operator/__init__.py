89d9ffebad03527d18158b9a6a64bbfcfd97f97e
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-05 02:40:14 +0200

    Update __init__.py

--- Operator/__init__.py
+++ Operator/__init__.py
@@ -1,5 +1,7 @@
 from .proxy_builder import CLIP_OT_proxy_builder
+from .tracker_settings import CLIP_OT_tracker_settings
 
 operator_classes = (
     CLIP_OT_proxy_builder,
+    CLIP_OT_tracker_settings,
 )
