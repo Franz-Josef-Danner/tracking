6853683f35d798dda15e6fed89a5e8542037b726
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-05 15:59:45 +0200

    Update clean_error_tracks.py

--- Operator/clean_error_tracks.py
+++ Operator/clean_error_tracks.py
@@ -17,7 +17,7 @@ def clean_error_tracks(context):
         track.select = False
 
     ee_prop = getattr(scene, "error_per_track", 1.0)
-    ee_initial = (ee_prop + 0.1) / 200
+    ee_initial = (ee_prop + 0.1) / 300
     threshold_factor = 0.9
     frame_range = (scene.frame_start, scene.frame_end)
 
