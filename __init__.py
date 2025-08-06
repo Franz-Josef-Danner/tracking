83980c2fb3df11e48b086138d5f106cd70229779
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-06 14:03:27 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -43,7 +43,7 @@ class CLIP_PT_kaiserlich_panel(bpy.types.Panel):
         layout.prop(scene, "frames_track")
         layout.prop(scene, "error_track")
         layout.separator()
-        layout.operator("CLIP.marker_helper_main", text="Track")
+        layout.operator("CLIP.detect", text="Track")
 
 # -------------------------------------
 # Registrierung der Klassen
