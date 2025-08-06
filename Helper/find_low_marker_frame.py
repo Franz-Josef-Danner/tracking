6474619010f503d682a2e282e1cbf6a6884d980d
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-06 17:17:57 +0200

    Update find_low_marker_frame.py

--- Helper/find_low_marker_frame.py
+++ Helper/find_low_marker_frame.py
@@ -6,7 +6,7 @@ def find_low_marker_frame(clip, min_marker=None, frame_start=None, frame_end=Non
     scene = bpy.context.scene
 
     if min_marker is None:
-        min_marker = scene.get("min_marker", 5)  # Fallback auf 5, falls nicht gesetzt
+        min_marker = scene.get("min_marker", 5)
 
     if frame_start is None:
         frame_start = clip.frame_start
@@ -18,8 +18,6 @@ def find_low_marker_frame(clip, min_marker=None, frame_start=None, frame_end=Non
     for frame in range(frame_start, frame_end + 1):
         count = 0
         for track in tracks:
-            if track.mute:
-                continue
             marker = track.markers.find_frame(frame)
             if marker:
                 count += 1
