cf6afb91f6365fdbcca292af5585d4940ceb7fc1
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-06 19:28:12 +0200

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -1,3 +1,5 @@
+import bpy
+
 from .marker_helper_main import CLIP_OT_marker_helper_main
 from .disable_proxy import CLIP_OT_disable_proxy
 from .enable_proxy import CLIP_OT_enable_proxy
@@ -6,22 +8,23 @@ from .set_test_value import set_test_value
 from .find_low_marker_frame import find_low_marker_frame
 from .jump_to_frame import jump_to_frame
 from .properties import RepeatEntry
+from .main import CLIP_OT_main  # Wichtig für Registrierung
 
-operator_classes = (
-    marker_helper_main,
-    enable_proxy,
-    disable_proxy,
+# Alle Klassen in eine Liste
+classes = (
+    RepeatEntry,
+    CLIP_OT_marker_helper_main,
+    CLIP_OT_enable_proxy,
+    CLIP_OT_disable_proxy,
+    CLIP_OT_main,
 )
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-        bpy.types.Scene.repeat_frame = bpy.props.CollectionProperty(type=RepeatEntry)
-        bpy.utils.register_class(CLIP_OT_main)  # oder dein gesamter Operator-Block
-
+    bpy.types.Scene.repeat_frame = bpy.props.CollectionProperty(type=RepeatEntry)
 
 def unregister():
+    del bpy.types.Scene.repeat_frame
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
-        bpy.utils.unregister_class(RepeatEntry)
-        bpy.utils.unregister_class(CLIP_OT_main)
