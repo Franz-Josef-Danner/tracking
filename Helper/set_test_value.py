a5a22070ab1c3f066cc5888a3a16db56d2f443ca
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-06 14:34:23 +0200

    Update set_test_value.py

--- Helper/set_test_value.py
+++ Helper/set_test_value.py
@@ -7,11 +7,9 @@ def set_test_value(scene):
     marker_adapt = marker_plus
     max_marker = int(marker_adapt * 1.1)
     min_marker = int(marker_adapt * 0.9)
-    marker_value = (marker_adapt + max_marker + min_marker) / 3
 
     scene["marker_adapt"] = marker_adapt
     scene["marker_max"] = max_marker
     scene["marker_min"] = min_marker
-    scene["marker_value"] = marker_value
 
     return marker_value
