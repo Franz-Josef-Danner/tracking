1183627e3ba7549a9072c4911ae2cc26b1642d01
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-08 01:12:47 +0200

    Update find_low_marker_frame.py

--- Helper/find_low_marker_frame.py
+++ Helper/find_low_marker_frame.py
@@ -9,7 +9,6 @@ def find_low_marker_frame(clip, marker_basis=20, frame_start=None, frame_end=Non
     if frame_end is None:
         frame_end = bpy.context.scene.frame_end
 
-    print(f"[MarkerCheck] Erwartete Mindestmarker pro Frame: {marker_basis}")
 
     for frame in range(frame_start, frame_end + 1):
         count = 0
@@ -18,11 +17,9 @@ def find_low_marker_frame(clip, marker_basis=20, frame_start=None, frame_end=Non
             if marker:
                 count += 1
 
-        print(f"[MarkerCheck] Frame {frame}: {count} aktive Marker")
 
         if count < marker_basis:
             print(f"[MarkerCheck] → Zu wenige Marker in Frame {frame}")
             return frame
 
-    print("[MarkerCheck] Kein Frame mit zu wenigen Markern gefunden.")
     return None
