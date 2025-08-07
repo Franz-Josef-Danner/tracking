74b32bfacec875d493922787715f5dac0de0def5
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-07 22:17:31 +0200

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -1,5 +1,7 @@
 import bpy
 
+from . import proxy_helper, clip_helper, ram_helper
+
 from .marker_helper_main import CLIP_OT_marker_helper_main
 from .disable_proxy import CLIP_OT_disable_proxy
 from .enable_proxy import CLIP_OT_enable_proxy
@@ -12,7 +14,16 @@ from .log_helper import write_log_entry
 from ..Helper.process_marker_path import process_marker_path
 from ..Helper.mute_invalid_segments import mute_invalid_segments
 from ..Helper.clear_path_on_split_tracks_segmented import clear_path_on_split_tracks_segmented
+from ..Helper.ram_helper import RamGuard, register_bpy_timer
 
+
+__all__ = [
+    "RamGuard",
+    "register_bpy_timer",
+    "proxy_helper",
+    "clip_helper",
+    "ram_helper",
+]
 # Alle Klassen in eine Liste
 classes = (
     RepeatEntry,
