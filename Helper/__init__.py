855ace25f35d150873763835187fbff13c5e35bc
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-07 22:53:42 +0200

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -1,6 +1,5 @@
 import bpy
 
-from . import proxy_helper, clip_helper  # <-- .ram_helper entfernt (liegt nicht in Operator)
 from .marker_helper_main import CLIP_OT_marker_helper_main
 from .disable_proxy import CLIP_OT_disable_proxy
 from .enable_proxy import CLIP_OT_enable_proxy
