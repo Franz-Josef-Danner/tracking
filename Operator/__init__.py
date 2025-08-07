c9f72d8cef10858e57d1b1f458c075af3c125895
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-07 22:56:40 +0200

    Update __init__.py

--- Operator/__init__.py
+++ Operator/__init__.py
@@ -1,31 +1,29 @@
+# tracking-efficent/Operator/__init__.py
 import bpy
 
-classes = ()
+from .proxy_builder import CLIP_OT_proxy_builder
+from .tracker_settings import CLIP_OT_tracker_settings
+from .detect import CLIP_OT_detect
+from .bidirectional_track import CLIP_OT_bidirectional_track
+from .clean_short_tracks import CLIP_OT_clean_short_tracks
+from .tracking_pipeline import CLIP_OT_tracking_pipeline
+from .clean_error_tracks import CLIP_OT_clean_error_tracks
+from .optimize_tracking_modal import CLIP_OT_optimize_tracking_modal
+from .main import CLIP_OT_main
+
+classes = (
+    CLIP_OT_proxy_builder,
+    CLIP_OT_tracker_settings,
+    CLIP_OT_detect,
+    CLIP_OT_bidirectional_track,
+    CLIP_OT_clean_short_tracks,
+    CLIP_OT_tracking_pipeline,
+    CLIP_OT_clean_error_tracks,
+    CLIP_OT_optimize_tracking_modal,
+    CLIP_OT_main,
+)
 
 def register():
-    global classes
-    # Lazy-Imports HIER drinnen, nicht oben!
-    from .proxy_builder import CLIP_OT_proxy_builder
-    from .tracker_settings import CLIP_OT_tracker_settings
-    from .detect import CLIP_OT_detect
-    from .bidirectional_track import CLIP_OT_bidirectional_track
-    from .clean_short_tracks import CLIP_OT_clean_short_tracks
-    from .tracking_pipeline import CLIP_OT_tracking_pipeline
-    from .clean_error_tracks import CLIP_OT_clean_error_tracks
-    from .optimize_tracking_modal import CLIP_OT_optimize_tracking_modal
-    from .main import CLIP_OT_main
-
-    classes = (
-        CLIP_OT_proxy_builder,
-        CLIP_OT_tracker_settings,
-        CLIP_OT_detect,
-        CLIP_OT_bidirectional_track,
-        CLIP_OT_clean_short_tracks,
-        CLIP_OT_tracking_pipeline,
-        CLIP_OT_clean_error_tracks,
-        CLIP_OT_optimize_tracking_modal,
-        CLIP_OT_main,
-    )
     for cls in classes:
         bpy.utils.register_class(cls)
 
