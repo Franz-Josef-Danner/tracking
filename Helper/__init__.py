504b8fc83fa1d75fea6cf9a1a64f03d69797bdf5
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-08 13:40:04 +0200

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -11,6 +11,7 @@ from .properties import RepeatEntry
 from ..Helper import ram_helper
 from ..Helper.ram_helper import RamGuard, register_bpy_timer
 from .grid_error_cleanup import grid_error_cleanup
+from .delete_all_keyframes import delete_all_keyframes
 
 __all__ = []
 
