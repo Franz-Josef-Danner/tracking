36326dd8491f2471392ff5c949afe61a0ee0e9f3
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-11 18:39:06 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -23,7 +23,6 @@ from .Operator.clean_short_tracks import CLIP_OT_clean_short_tracks
 from .Operator.clean_error_tracks import CLIP_OT_clean_error_tracks
 from .Operator.optimize_tracking_modal import CLIP_OT_optimize_tracking_modal
 from .Operator.main import CLIP_OT_main
-from .Helper import solve_camera_helper as _solve_helper
 
 # Importiere PropertyGroup
 from .Helper.properties import RepeatEntry  # ✅ NEU
@@ -66,7 +65,6 @@ classes = (
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-    _solve_helper.register() 
 
     # Custom Property für Frame-Tracking
     bpy.types.Scene.repeat_frame = bpy.props.CollectionProperty(type=RepeatEntry)  # ✅ NEU
