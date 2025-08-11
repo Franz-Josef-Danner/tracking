0edcbbb4f504d26f57db27f3c44bc477d8f81254
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-12 00:18:15 +0200

    Update main.py

--- Operator/main.py
+++ Operator/main.py
@@ -145,7 +145,7 @@ class CLIP_OT_main(bpy.types.Operator):
 
                 scene = context.scene
                 scene["solve_status"] = "pending"
-                time.sleep(1)
+                time.sleep(2)
                 bpy.ops.clip.watch_solve('INVOKE_DEFAULT')
                 self._step = 3
                 return {'PASS_THROUGH'}
