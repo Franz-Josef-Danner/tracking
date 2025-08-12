0f5d5e1e76b19cd7db219e91cd9b3e047734b8fd
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-12 23:17:19 +0200

    Update solve_camera_helper.py

--- Helper/solve_camera_helper.py
+++ Helper/solve_camera_helper.py
@@ -1,6 +1,6 @@
 import bpy
 from bpy.types import Operator
-from .refine_on_high_error import run_refine_on_high_error
+from .refine_high_error import run_refine_on_high_error
 
 def _clip_override(context):
     """Sichere CLIP_EDITOR-Overrides ermitteln (immer frisch abrufen)."""
