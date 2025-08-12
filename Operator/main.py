21644d5f088646729f8e26df70e5fd096ea66a52
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-12 20:33:54 +0200

    Update main.py

--- Operator/main.py
+++ Operator/main.py
@@ -4,7 +4,7 @@ import time
 from ..Helper.find_low_marker_frame import find_low_marker_frame
 from ..Helper.jump_to_frame import jump_to_frame
 from ..Helper.properties import RepeatEntry  # <- wichtig!
-from ..Helper.solve_camera_helper import CLIP_OT_solve_watch_clean
+from ..Helper.solve_camera_helper import CLIP_OT_solve_watch_clean, run_solve_watch_clean
 
 class CLIP_OT_main(bpy.types.Operator):
     bl_idname = "clip.main"
