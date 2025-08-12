b2570b9c3f72fda787c86d7f1053e619a3d83c60
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-13 01:53:44 +0200

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -5,7 +5,6 @@ from .disable_proxy import CLIP_OT_disable_proxy
 from .enable_proxy import CLIP_OT_enable_proxy
 from .error_value import error_value
 from .set_test_value import set_test_value
-from .find_low_marker_frame import find_low_marker_frame
 from .jump_to_frame import jump_to_frame
 from .properties import RepeatEntry
 from .log_helper import write_log_entry
