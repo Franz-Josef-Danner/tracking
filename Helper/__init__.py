d79dbfc830c815e6156a3efa9154a27066d0eb76
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-13 13:40:01 +0200

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -13,7 +13,7 @@ from .segments import track_has_internal_gaps, get_track_segments
 from .naming import _safe_name
 from .mute_ops import mute_marker_path, mute_after_last_marker, mute_unassigned_markers
 from .split_cleanup import clear_path_on_split_tracks_segmented, recursive_split_cleanup
-from .refine_high_error import run_refine_on_high_error
+from .refine_high_error import CLIP_OT_refine_on_high_error
 from .marker_adapt_helper import register as reg_adapt, unregister as unreg_adapt
 
 
@@ -27,6 +27,7 @@ classes = (
     CLIP_OT_enable_proxy,
     CLIP_OT_disable_proxy,
     CLIP_OT_clean_tracks_projection_error,
+    CLIP_OT_refine_on_high_error,
 )
 
 def register():
