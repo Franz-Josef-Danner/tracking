9d7a3683e50221d67a40927fd2462d30878227ea
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-13 22:56:04 +0200

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -7,7 +7,8 @@ from .set_test_value import set_test_value
 from .properties import RepeatEntry
 from .log_helper import write_log_entry
 from .find_low_marker_frame import run_find_low_marker_frame
-from .jump_to_frame_helper import jump_to_frame
+from .jump_to_frame import jump_to_frame_helper
+from .detect import run_detect_adaptive
 from .find_max_marker_frame import get_active_marker_counts_sorted
 from .projection_cleanup_builtin import builtin_projection_cleanup, find_clip_window
 from .multiscale_temporal_grid_clean import multiscale_temporal_grid_clean
