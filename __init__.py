fcceef809c9e06454afc567a3cb3712b2607427b
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-13 23:01:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,7 +16,6 @@ from .Helper.disable_proxy import CLIP_OT_disable_proxy
 from .Helper.enable_proxy import CLIP_OT_enable_proxy
 from .Operator.solve_camera import CLIP_OT_solve_watch_clean, run_solve_watch_clean
 from .Operator.clean_short_tracks import CLIP_OT_clean_short_tracks
-from .Operator.clean_error_tracks import CLIP_OT_clean_error_tracks
 from .Operator.optimize_tracking_modal import CLIP_OT_optimize_tracking_modal
 from .Operator.main import CLIP_OT_main
 from .Operator.marker_helper_main import CLIP_OT_marker_helper_main
@@ -48,7 +47,6 @@ classes = (
     CLIP_OT_enable_proxy,
     CLIP_OT_solve_watch_clean,
     CLIP_OT_clean_short_tracks,
-    CLIP_OT_clean_error_tracks,
     CLIP_OT_optimize_tracking_modal,
     CLIP_OT_main,
     CLIP_OT_marker_helper_main,
