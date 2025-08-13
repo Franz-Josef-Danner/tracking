03214de72c5c1b7931f6144c906ac6e074b24e63
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-13 21:52:46 +0200

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -11,6 +11,7 @@ from .projection_cleanup_builtin import builtin_projection_cleanup, find_clip_wi
 from .multiscale_temporal_grid_clean import multiscale_temporal_grid_clean
 from .segments import track_has_internal_gaps, get_track_segments
 from .naming import _safe_name
+from .tracker_settings_helper import apply_tracker_settings
 from .main_to_adapt import main_to_adapt
 from .mute_ops import mute_marker_path, mute_after_last_marker, mute_unassigned_markers
 from .split_cleanup import clear_path_on_split_tracks_segmented, recursive_split_cleanup
