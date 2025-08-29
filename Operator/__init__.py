33165896583de5436d71e6a4c50c73f902d8a2da
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-08-29 14:32:18 +0200

    Update __init__.py

--- Operator/__init__.py
+++ Operator/__init__.py
@@ -1,6 +1,6 @@
 import bpy
 
-from .Operator.tracking_coordinator import register as _reg_coord, unregister as _unreg_coord
+from .tracking_coordinator import CLIP_OT_tracking_coordinator
 
 classes = (
     CLIP_OT_tracking_coordinator,
