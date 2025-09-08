7844340969fdcbe9d5d60de204c94545fe062f21
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-09-08 10:11:51 +0200

    Delete Helper/properties.py

--- Helper/properties.py
+++
@@ -1,7 +0,0 @@
-# properties.py
-import bpy
-from bpy.props import StringProperty, IntProperty
-
-class RepeatEntry(bpy.types.PropertyGroup):
-    frame: StringProperty(name="Frame")
-    count: IntProperty(name="Count")
