cb03799910af7caa13520bbacc9eb94f0c89c942
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-01 19:52:56 +0000

    Aktualisiere den minimalen Schwellenwert in der Funktion detect_features_multipass auf 0.1 für verbesserte Genauigkeit.

--- helpers/detect_helper.py
+++ helpers/detect_helper.py
@@ -36,7 +36,7 @@ def get_clip_from_area(area):
 def detect_features_multipass(
     context,
     start_threshold=1.0,
-    min_threshold=0.0001,
+    min_threshold=0.1,
     factor=0.5,
     max_passes=32,
     remove_duplicates=True,
