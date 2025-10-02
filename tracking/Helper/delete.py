9803159bc851b938707bd7161a90702ca8a1dc0c
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-02 15:15:03 +0000

    Entferne nicht verwendete Hilfsfunktionen und bereinige den Code in den Tracking-Hilfsmodulen zur Verbesserung der Wartbarkeit und Reduzierung der Komplexität.

--- tracking/Helper/delete.py
+++
@@ -1,8 +0,0 @@
-import bpy
-
-def delete_marker(track_marker_tuple, frame: int):
-    track, marker = track_marker_tuple
-    try:
-        track.markers.delete_frame(frame)
-    except Exception as e:
-        print(f"Fehler beim Löschen Marker @Frame {frame}: {e}")
