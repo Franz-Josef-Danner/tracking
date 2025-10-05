cdadbb63e7ff5d362f30ce24412e84b471c0c4a1
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-05 21:31:37 +0000

    Refactor Korridor Parameter: Setze den Standardwert für finish_threshold auf 0.001 und passe die minimale Grenze für corridor_tr_factor auf 0.0001 an.

--- Operator/operator.py
+++ Operator/operator.py
@@ -38,7 +38,7 @@ class KAISERLICHTRACKER_OT_detect_cycle(bpy.types.Operator):
 
     finish_threshold: bpy.props.FloatProperty(  # type: ignore
         name="Finish Threshold (tr <)",
-        default=0.01,
+        default=0.001,
         min=0.0001,
         description="Schwellwert unter den der Threshold fallen muss um im Korridor final zu beenden"
     )
@@ -46,7 +46,7 @@ class KAISERLICHTRACKER_OT_detect_cycle(bpy.types.Operator):
     corridor_tr_factor: bpy.props.FloatProperty(  # type: ignore
         name="Korridor Threshold Faktor",
         default=0.15,
-        min=0.001,
+        min=0.0001,
         max=0.99,
         description="Faktor zur Multiplikation von tr wenn (ug < am < og)"
     )
