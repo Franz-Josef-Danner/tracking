2304e7ff22d9760f558f8e1872a73d81af49ea52
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-09 00:42:21 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -20,6 +20,7 @@ classes = (
 )
 
 def register():
+    ui.register()
     for cls in classes:
         bpy.utils.register_class(cls)
     # Property für Eingabefeld
@@ -32,6 +33,7 @@ def register():
     )
 
 def unregister():
+    ui.unregister()
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
     del bpy.types.Scene.kaiserlich_markers_per_frame
