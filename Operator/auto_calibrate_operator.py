47615553183f3531cb6e8d0e1dc285113729877a
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-13 14:05:39 +0200

    Fix step value from +0.90 to +0.95

--- Operator/auto_calibrate_operator.py
+++ Operator/auto_calibrate_operator.py
@@ -35,7 +35,7 @@ class KAISERLICHTRACKER_OT_auto_calibrate(bpy.types.Operator):
         "kaiserlich_perspective_thresh",
     ]
 
-    _steps = [-0.95, +0.90, -0.80, +0.70, -0.60, +0.50, -0.40, +0.30, -0.20, +0.10, -0.05, +0.02, -0.01]
+    _steps = [-0.95, +0.95, -0.80, +0.70, -0.60, +0.50, -0.40, +0.30, -0.20, +0.10, -0.05, +0.02, -0.01]
 
     # ---------------------------------------
     # Hilfsfunktionen
