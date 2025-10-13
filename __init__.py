1d25bd7879c51ed923a32553429a2373097e32bf
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-13 19:24:53 +0200

    Add detect_adupt operator to the classes

--- __init__.py
+++ __init__.py
@@ -13,10 +13,12 @@ from .UI.ui import KAISERLICHTRACKER_PT_panel
 from .Operator.detect_operator import KAISERLICHTRACKER_OT_detect_cycle
 from .Operator.track_operator import KAISERLICHTRACKER_OT_track_cycle
 from .Operator.auto_calibrate_operator import KAISERLICHTRACKER_OT_auto_calibrate
+from .Operator.detect_adupt_operator import KAISERLICHTRACKER_OT_detect_adupt
 classes = (
     KAISERLICHTRACKER_OT_detect_cycle,
     KAISERLICHTRACKER_OT_track_cycle,
     KAISERLICHTRACKER_OT_auto_calibrate,
+    KAISERLICHTRACKER_OT_detect_adupt
     KAISERLICHTRACKER_PT_panel,
 )
 
