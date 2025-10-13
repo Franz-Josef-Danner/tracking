397a7cb29d05695bd0390175973fc76eca0f9adf
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-13 22:03:49 +0200

    Update detect_adapt_operator.py

--- Operator/detect_adapt_operator.py
+++ Operator/detect_adapt_operator.py
@@ -8,8 +8,8 @@ from ..Helper.delete import delete_tracks_by_names
 
 
 class KAISERLICHTRACKER_OT_detect_adapt(bpy.types.Operator):
-    bl_idname = "kaiserlich_tracker.detect_cycle"
-    bl_label = "Detect Zyklus (einmalig)"
+    bl_idname = "kaiserlich_tracker.detect_adapt"  # ← RICHTIG
+    bl_label = "Detect Adapt (einmalig)"
     bl_description = (
         "Führt eine einmalige Marker-Detektion aus: Bootstrap → Detect → Cleanup → Selektieren."
     )
