924b57086e8ec781bff9987f6ae6c2551561939f
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-13 21:46:18 +0200

    Fix typo in operator name for detect_adapt

--- UI/ui.py
+++ UI/ui.py
@@ -23,7 +23,7 @@ class KAISERLICHTRACKER_PT_panel(bpy.types.Panel):
         col.operator("kaiserlich_tracker.detect_cycle", text="Detect Cycle", icon="VIEWZOOM")
         col.operator("kaiserlich_tracker.track_cycle", text="Track Cycle", icon="TRACKING")
         col.operator("kaiserlich_tracker.auto_calibrate", text="Auto-Calibrate Thresholds", icon="MOD_WAVE")
-        col.operator("kaiserlich_tracker.detect_adupt", text="Run Detect Adupt", icon="FILE_TICK")
+        col.operator("kaiserlich_tracker.detect_adapt", text="Run Detect Adapt", icon="FILE_TICK")
 
         layout.separator()
 
