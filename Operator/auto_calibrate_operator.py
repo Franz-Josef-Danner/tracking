f1d5db64170f321a14fa25c2f6ff52144b189e5a
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-15 01:14:49 +0200

    Update calibration step value from -0.20 to -0.30

--- Operator/auto_calibrate_operator.py
+++ Operator/auto_calibrate_operator.py
@@ -32,7 +32,7 @@ class KAISERLICHTRACKER_OT_auto_calibrate(bpy.types.Operator):
         "kaiserlich_perspective_thresh",
     ]
 
-    _steps = [-0.95, +0.95, -0.20, +0.05, -0.05, +0.01, -0.01]
+    _steps = [-0.95, +0.95, -0.30, +0.05, -0.05, +0.01, -0.01]
 
     # ---------------------------------------
     # Hilfsfunktionen
