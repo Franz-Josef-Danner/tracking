0a91d0de33a153a5f6dcd7979dec54494bc78c34
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-21 04:21:01 +0200

    Change Master button icon to PLUGIN

--- UI/ui.py
+++ UI/ui.py
@@ -21,7 +21,7 @@ class KAISERLICHTRACKER_PT_panel(bpy.types.Panel):
         col.prop(scene, "kaiserlich_markers_per_frame", text="Marker per Frame")
 
         # ▶️ Buttons
-        col.operator("kaiserlich_tracker.master_operator", text="Master", icon="cup")
+        col.operator("kaiserlich_tracker.master_operator", text="Master", icon="PLUGIN")
         col.operator("kaiserlich_tracker.auto_calibrate", text="Auto-Calibrate Thresholds", icon="MOD_WAVE")
         col.operator("kaiserlich_tracker.detect_adapt", text="Run Detect Adapt", icon="FILE_TICK")
         col.operator("kaiserlich_tracker.track_cycle_backwards", text="Track Cycle (Backwards)", icon="TRACKING")
