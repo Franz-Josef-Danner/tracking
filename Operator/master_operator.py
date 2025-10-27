e89c6437b0aa661717e42da49d9bc747b7b837cc
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-27 14:54:23 +0100

    Remove master operator and registration functions
    Removed the master operator class and its registration functions.

--- Operator/master_operator.py
+++ Operator/master_operator.py
@@ -1,18 +1,1 @@
-import bpy
 
-
-class KAISERLICHTRACKER_OT_master_operator(bpy.types.Operator):
-
-
-# Registration
-classes = (
-    KAISERLICHTRACKER_OT_master_operator,
-)
-
-def register():
-    for cls in classes:
-        bpy.utils.register_class(cls)
-
-def unregister():
-    for cls in reversed(classes):
-        bpy.utils.unregister_class(cls)
