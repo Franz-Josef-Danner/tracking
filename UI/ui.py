5c74ee2a5deac0ee2017dd17df89ec69f1e11111
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-28 19:53:42 +0100

    Rename operator button and add deep test option

--- UI/ui.py
+++ UI/ui.py
@@ -23,7 +23,8 @@ class KAISERLICHTRACKER_PT_panel(bpy.types.Panel):
         
         # ▶️ Buttons
         col.operator("kaiserlich_tracker.master_operator", text="Master", icon="PLUGIN")
-        col.operator("kaiserlich_tracker.shorttest_operator", text="Auto-Calibrate Thresholds", icon="MOD_WAVE")
+        col.operator("kaiserlich_tracker.shorttest_operator", text="Short Test Thresholds", icon="MOD_WAVE")
+        col.operator("kaiserlich_tracker.deep_test", text="Deep Test Thresholds", icon="MOD_WAVE")
         col.operator("kaiserlich_tracker.detect_adapt", text="Run Detect Adapt", icon="FILE_TICK")
         col.operator("kaiserlich_tracker.track_cycle_backwards", text="Track Cycle (Backwards)", icon="TRACKING")
         col.operator("kaiserlich_tracker.track_cycle", text="Track Cycle (Forward)", icon="TRACKING")
