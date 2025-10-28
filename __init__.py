b678772924987d74caf79075bb18a5ea7c4b0645
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-10-28 19:56:54 +0100

    Add deep test operator to the module

--- __init__.py
+++ __init__.py
@@ -12,12 +12,14 @@ import bpy
 from .UI.ui import KAISERLICHTRACKER_PT_panel
 from .Operator.track_operator import KAISERLICHTRACKER_OT_track_cycle
 from .Operator.shorttest_operator import KAISERLICHTRACKER_OT_shorttest_operator
+from .Operator.deep_test_operator import KAISERLICHTRACKER_OT_deep_test_operator
 from .Operator.detect_adapt_operator import KAISERLICHTRACKER_OT_detect_adapt
 from .Operator.track_operator_backwards import KAISERLICHTRACKER_OT_track_cycle_backwards
 from .Operator.master_operator import KAISERLICHTRACKER_OT_master_operator
 classes = (
     KAISERLICHTRACKER_OT_track_cycle,
     KAISERLICHTRACKER_OT_shorttest_operator,
+    KAISERLICHTRACKER_OT_deep_test_operator,
     KAISERLICHTRACKER_OT_detect_adapt,
     KAISERLICHTRACKER_OT_track_cycle_backwards,
     KAISERLICHTRACKER_OT_master_operator,
