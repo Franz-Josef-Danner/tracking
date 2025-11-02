6e4abba808462b79232f2bbda8d0d00df83226c2
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-03 00:47:07 +0100

    Update import paths for backward operator script

--- Operator/Master/master_track_operator_backwards.py
+++ Operator/Master/master_track_operator_backwards.py
@@ -6,13 +6,13 @@ from collections import deque
 # ------------------------------------------------------------
 # Helper-Importe
 # ------------------------------------------------------------
-from ..Helper.formula_helper import apply_formula_on_selected_tracks
-from ..Helper.playhead_helper import reset_to_frame
-from ..Helper.scene import get_end_frame, get_start_frame as scene_get_start_frame
-from ..Helper.find_clip_editor_area import find_clip_editor_area
-from ..Helper.selection_helper import collect_selected_track_names
-from ..Helper.filter_active_tracks import filter_active_tracks_at_frame
-from ..Helper.track_markers_helper import track_markers_with_override
+from ...Helper.formula_helper import apply_formula_on_selected_tracks
+from ...Helper.playhead_helper import reset_to_frame
+from ...Helper.scene import get_end_frame, get_start_frame as scene_get_start_frame
+from ...Helper.find_clip_editor_area import find_clip_editor_area
+from ...Helper.selection_helper import collect_selected_track_names
+from ...Helper.filter_active_tracks import filter_active_tracks_at_frame
+from ...Helper.track_markers_helper import track_markers_with_override
 
 
 # ------------------------------------------------------------
