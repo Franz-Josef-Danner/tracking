fbf885e58eacfe77bfdbba6b8f1cdb7857bea394
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-03 04:21:09 +0100

    Update and rename master_operator.py to master_cycle_operator.py

--- Operator/Master/master_cycle_operator.py
+++ Operator/Master/master_cycle_operator.py
@@ -1,13 +1,13 @@
-# Operator/master_operator.py
+# Operator/master_cycle_operator.py
 import bpy
 from bpy.types import Operator, Context
 
 # ---- Helper-Importe ---------------------------------------------------------
-from ..Helper.low_marker_frame import find_first_weak_frame
+from ...Helper.low_marker_frame import find_first_weak_frame
 
-class KAISERLICHTRACKER_OT_master_operator(Operator):
+class KAISERLICHTRACKER_OT_master_cycle_operator(Operator):
     """Master Operator – setzt Playhead auf Frame mit den wenigsten aktiven Markern"""
-    bl_idname = "kaiserlich_tracker.master_operator"
+    bl_idname = "kaiserlich_tracker.master_cycle_operator"
     bl_label = "Master Operator"
     bl_description = "Setzt den Playhead auf den ersten Frame mit der geringsten Markeranzahl"
     bl_options = {'REGISTER', 'UNDO'}
@@ -61,7 +61,7 @@ class KAISERLICHTRACKER_OT_master_operator(Operator):
 
 # ---- Registrierung ----------------------------------------------------------
 def register():
-    bpy.utils.register_class(KAISERLICHTRACKER_OT_master_operator)
+    bpy.utils.register_class(KAISERLICHTRACKER_OT_master_cycle_operator)
 
 def unregister():
-    bpy.utils.unregister_class(KAISERLICHTRACKER_OT_master_operator)
+    bpy.utils.unregister_class(KAISERLICHTRACKER_OT_master_cycle_operator)
