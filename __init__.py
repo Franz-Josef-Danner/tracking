543c43262420cae724b5fef9291dbbed59f584d0
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-03 18:11:48 +0100

    Add properties for frames per track and max error value

--- __init__.py
+++ __init__.py
@@ -54,7 +54,20 @@ def register():
         min=1,
         soft_min=1,
     )
-
+    bpy.types.Scene.kaiserlich_frames_per_track = bpy.props.IntProperty(
+        name="Frames per Track",
+        description="Mindestanzahl an Frames, die ein Track haben muss, um beim Cleanup nicht gelöscht zu werden",
+        default=25,
+        min=0,
+        soft_min=0,
+    )
+    bpy.types.Scene.max_error_value = bpy.props.IntProperty(
+        name="Max error Value",
+        description="Jeder track mit einem solve error über diesem wert wird ausgefiltert",
+        default=2,
+        min=0.5,
+        soft_min=1,
+    )
     # UI-Properties registrieren
     try:
         from .UI import ui
