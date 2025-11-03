ba34174065064c2e18e4236941c91287b027c2bc
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-03 15:57:43 +0100

    Add List and Set imports to master_cycle_operator.py

--- Operator/Master/master_cycle_operator.py
+++ Operator/Master/master_cycle_operator.py
@@ -1,6 +1,6 @@
 # Operator/master_cycle_operator.py
 import bpy
-from bpy.types import Optional, Operator, Context
+from bpy.types import Optional, Operator, Context, List, Set
 
 # ---- Helper-Importe ---------------------------------------------------------
 from ...Helper.low_marker_frame import find_first_weak_frame
