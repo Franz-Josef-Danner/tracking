97d05486933b62f24505a59d86af4c80f802f456
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-04 19:45:36 +0100

    Fix operator name in master_operator.py

--- Operator/master_operator.py
+++ Operator/master_operator.py
@@ -64,7 +64,7 @@ class KAISERLICHTRACKER_OT_master_operator(Operator):
                 return {'CANCELLED'}
 
             # Start
-            bpy.ops.kaiserlich_tracker.master_deep_test_operator('INVOKE_DEFAULT')
+            bpy.ops.kaiserlichtracker.master_deep_test_operator('INVOKE_DEFAULT')
             print("[Kaiserlich Tracker][Master] DeepTest erfolgreich gestartet.")
         except Exception as ex:
             print(f"[Kaiserlich Tracker][Master] ⚠️ Fehler beim Starten des DeepTest: {ex!r}")
