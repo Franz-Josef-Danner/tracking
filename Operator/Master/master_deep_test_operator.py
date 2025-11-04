c089952d3ab172446bdc70d5e8fef754c3c56114
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-04 19:55:11 +0100

    Fix comment formatting in master_deep_test_operator.py

--- Operator/Master/master_deep_test_operator.py
+++ Operator/Master/master_deep_test_operator.py
@@ -1,4 +1,4 @@
-Operator/Master/master_deep_test_operator.py
+# Operator/Master/master_deep_test_operator.py
 import bpy
 from bpy.types import Operator, Context
 from dataclasses import dataclass, field
