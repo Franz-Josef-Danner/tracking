655245b15bba8e8fedcab3e55b5bacfb91f60c76
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-04 19:37:55 +0100

    Rename bl_idname for master deep test operator

--- Operator/Master/master_deep_test_operator.py
+++ Operator/Master/master_deep_test_operator.py
@@ -33,7 +33,7 @@ class DeepTestState:
 
 
 class KAISERLICHTRACKER_OT_master_deep_test_operator(Operator):
-    bl_idname = "kaiserlichtracker.deep_test"
+    bl_idname = "kaiserlichtracker.master_deep_test_operator"
     bl_label = "Kaiserlich Tracker: Deep Test"
     bl_options = {'REGISTER', 'UNDO'}
 
