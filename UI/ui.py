0fb803fd28079fb7c4e05e6a63350161ccff7802
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-05 01:07:12 +0100

    Add slider for kaiserlich_progress_value in UI

--- UI/ui.py
+++ UI/ui.py
@@ -85,6 +85,8 @@ class KAISERLICHTRACKER_PT_panel(bpy.types.Panel):
         col = layout.column(align=True)
         col.scale_y = 1.4
         col.prop(scene, "kaiserlich_progress_title", text=f"{scene.kaiserlich_progress_value * 100:.1f}%")
+        col.prop(scene, "kaiserlich_progress_value", slider=True)
+
 # ==========================================================
 # Registrierung der UI-Properties
 # ==========================================================
