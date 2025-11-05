b0eb281408d54be93cab9cb1d5f5bc939f0c7405
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-05 16:07:42 +0100

    Refactor progress calculation and display format

--- Operator/Master/master_deep_test_operator.py
+++ Operator/Master/master_deep_test_operator.py
@@ -254,10 +254,10 @@ class KAISERLICHTRACKER_OT_master_deep_test_operator(Operator):
 
         vale = min(100.0, 100.0 - (((math.log10(max(0.00001, converter) * 100000.0) - 0.205) * 1.03) * 20.0))
         if vale < 100:
-            total = max(0, min(100, (((step * 1.25) + 1) * 17) - ((100 - (vale-1))/6)))
-            scene.kaiserlich_progress_step  = f"Total: {int(total)}%"
+            total = max(0, min(100, (((step * 1.224) + 1) * 17) - ((100 - (vale-1))/6)))
+            scene.kaiserlich_progress_step  = f"{int(total)}%"
         # 🟢 Fortschrittswerte in Szene-Properties schreiben (für UI-Refresh)
-        scene.kaiserlich_progress_title = f"Single Tests: {int(vale)}%"
+        scene.kaiserlich_progress_title = f"{int(vale)}%"
         try:
             scene.kaiserlich_converter = converter
             # 🟢 Nach Aktualisierung gezielt UI-Region refreshen
