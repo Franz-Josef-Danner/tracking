2b21c7a6f38493cdb0083adcd4daeb5c7e837a7d
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-06 00:01:15 +0100

    Fix operator namespace in UI buttons

--- UI/ui.py
+++ UI/ui.py
@@ -31,13 +31,13 @@ class KAISERLICHTRACKER_PT_panel(bpy.types.Panel):
         # Zwei Buttons nebeneinander
         row = col.row(align=True)
         row.operator("kaiserlich_tracker.master_shorttest_operator", text="Short Test Thresholds", icon="VIEWZOOM")
-        row.operator("kaiserlich_tracker.master_deep_test_operator", text="Deep Test Thresholds", icon="ZOOM_IN")
+        row.operator("kaiserlichtracker.master_deep_test_operator", text="Deep Test Thresholds", icon="ZOOM_IN")
 
         col.operator("kaiserlich_tracker.master_detect_adapt", text="Run Detect Adapt", icon="STICKY_UVS_DISABLE")
         
         row = col.row(align=True)
         row.operator("kaiserlich_tracker.master_track_cycle_backwards", text="Track Cycle (Backwards)", icon="TRACKING_BACKWARDS")
-        row.operator("kaiserlich_tracker.cycle_operator", text="Track Cycle (Forward)", icon="TRACKING_FORWARDS")
+        row.operator("kaiserlichtracker.cycle_operator", text="Track Cycle (Forward)", icon="TRACKING_FORWARDS")
 
         col.operator("kaiserlich_tracker.master_resolve_operator", text="Run resolve camera", icon="STICKY_UVS_DISABLE")
      
