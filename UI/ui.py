1b888ddbde5a0a3ce409ca0d7dcec142a058983d
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-06 22:32:19 +0100

    Fix operator name for cleaning error tracks

--- UI/ui.py
+++ UI/ui.py
@@ -41,7 +41,7 @@ class KAISERLICHTRACKER_PT_panel(bpy.types.Panel):
 
         row = col.row(align=True)
         row.operator("kaiserlich_tracker.master_resolve_operator", text="solve camera", icon="OUTLINER_OB_CAMERA")
-        row.operator("kaiserlich_tracker.clean_error_operator", text="delet error track", icon="ERROR")
+        row.operator("kaiserlichtracker.clean_error_operator", text="delet error track", icon="ERROR")
       
         layout.separator()
 
