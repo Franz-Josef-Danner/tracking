0c000637f7acd767ff06cd7b8871f913b6f2a7e5
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-06 04:47:59 +0100

    Change spike_threshold to a lower value

--- Helper/track_quality_metrics.py
+++ Helper/track_quality_metrics.py
@@ -50,7 +50,7 @@ def compute_track_quality_metrics(
     context: bpy.types.Context,
     *,
     min_len_for_long=25,
-    spike_threshold=3,
+    spike_threshold=0.01,
     velocity_thresh=0.008,
     accel_thresh=0.020,
 ):
