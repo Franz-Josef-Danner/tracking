39488ad0632e4e209ecd6b2d2bb4db998f9c5e84
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-07 13:49:32 +0100

    Update button labels in UI for clarity

--- UI/ui.py
+++ UI/ui.py
@@ -26,19 +26,19 @@ class KAISERLICHTRACKER_PT_panel(bpy.types.Panel):
 
         # ▶️ Buttons
         col = layout.column(align=True)
-        col.operator("kaiserlich_tracker.master_operator", text="Master", icon="SYSTEM")
+        col.operator("kaiserlich_tracker.master_operator", text="send everyone", icon="SYSTEM")
 
-        col.operator("kaiserlichtracker.master_deep_test_operator", text="Deep Test Thresholds", icon="ZOOM_IN")
+        col.operator("kaiserlichtracker.master_deep_test_operator", text="find their weakness", icon="ZOOM_IN")
 
-        col.operator("kaiserlich_tracker.master_detect_adapt", text="Run Detect Adapt", icon="STICKY_UVS_DISABLE")
+        col.operator("kaiserlich_tracker.master_detect_adapt", text="deploy troops", icon="STICKY_UVS_DISABLE")
         
         row = col.row(align=True)
-        row.operator("kaiserlich_tracker.master_track_cycle_backwards", text="Track Cycle (Backwards)", icon="TRACKING_BACKWARDS")
-        row.operator("kaiserlich_tracker.master_track_cycle", text="Track Cycle (Forward)", icon="TRACKING_FORWARDS")
+        row.operator("kaiserlich_tracker.master_track_cycle_backwards", text="<-- Att(r)ack", icon="TRACKING_BACKWARDS")
+        row.operator("kaiserlich_tracker.master_track_cycle", text="Att(r)ack →", icon="TRACKING_FORWARDS")
 
         row = col.row(align=True)
-        row.operator("kaiserlich_tracker.master_resolve_operator", text="solve camera", icon="OUTLINER_OB_CAMERA")
-        row.operator("kaiserlich_tracker.clean_error_operator", text="delet error track", icon="ERROR")
+        row.operator("kaiserlich_tracker.master_resolve_operator", text="Survey the Field", icon="OUTLINER_OB_CAMERA")
+        row.operator("kaiserlich_tracker.clean_error_operator", text="Purge the Fallen", icon="ERROR")
       
         layout.separator()
 
