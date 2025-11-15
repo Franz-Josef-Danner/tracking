3fbb20e8d3f3123159c9825b879b6888a35c7ae4
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-15 03:50:56 +0000

    Verbessere die Berechnung der Suchgröße: Begrenze die Suchminima und -maxima auf einen Maximalwert von 100, um Überläufe zu vermeiden.

--- Helper/adapt_search_size.py
+++ Helper/adapt_search_size.py
@@ -49,8 +49,8 @@ def _apply_search_size(marker, pattern_size: Vector, scale_factor: float = 2.0):
     size = pattern_size * scale_factor
     half = size * 0.5
 
-    marker.search_min = Vector((-half.x, -half.y))
-    marker.search_max = Vector((+half.x, +half.y))
+    marker.search_min = Vector((min(100,-half.x), min(100,-half.y)))
+    marker.search_max = Vector((min(100,+half.x), min(100,+half.y)))
 
 
 def adapt_search_size_for_calibrate_tracks(context):
