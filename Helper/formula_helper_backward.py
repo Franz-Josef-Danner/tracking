9a7f841f3513dc789f98040b2ca96f3357f5709f
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-17 23:51:48 +0100

    Fix perspective threshold calculation in formula_helper

--- Helper/formula_helper_backward.py
+++ Helper/formula_helper_backward.py
@@ -166,9 +166,9 @@ def apply_formula_on_selected_tracks_backwards(context: bpy.types.Context, max_f
         # --- 2) Perspective global & per Marker einmalig berechnen ---
         _, global_p_dev, per_marker_dev = _detect_perspective_motion(
             marker_positions,
-            getattr((scene, "kaiserlich_perspective_thresh", 0.002) + 30) / 100
+            getattr((scene, "kaiserlich_perspective_thresh", 0.002) + 40) / 100
         )
-        perspective_thresh = getattr((scene, "kaiserlich_perspective_thresh", 0.002) + 30) / 100
+        perspective_thresh = getattr((scene, "kaiserlich_perspective_thresh", 0.002) + 40) / 100
         if global_p_dev > perspective_thresh:
             global_model = "Perspective"
 
