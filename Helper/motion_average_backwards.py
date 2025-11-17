84a592345f4f45b89569bdd0a8eb5b1382238441
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-17 22:50:44 +0100

    Adjust threshold calculations in motion_average_backwards

--- Helper/motion_average_backwards.py
+++ Helper/motion_average_backwards.py
@@ -245,13 +245,13 @@ def get_from_selected_tracks_backwards(
         dy_var_mean = sum(dy_var_accum) / len(dy_var_accum) if dy_var_accum else 0.0
         rel_var_mean = sum(rel_var_accum) / len(rel_var_accum) if rel_var_accum else 0.0
 
-        scene["kaiserlich_rot_thresh_x"] = 500 - (((dx_var_mean) * 0.0036) * 100000)
-        scene["kaiserlich_rot_thresh_y"] = 800 - (((dy_var_mean) * 0.0067) * 100000)
-        scene["kaiserlich_scale_thresh_min"] = 30 - (((rel_var_mean) * 0.00024) * 100000)
-        scene["kaiserlich_scale_thresh_max"] = 10 - (((rel_var_mean) * 0.00012) * 100000)
-        scene["kaiserlich_rot_scale_thresh_rot"] = 800 - ((((dx_var_mean * 0.0036) + (dy_var_mean * 0.0067)) / 2) * 100000)
-        scene["kaiserlich_rot_scale_thresh_scale"] =  20 - (((rel_var_mean) * 0.00018) * 100000)
-        scene["kaiserlich_perspective_thresh"] = 1000 - min(1000, (global_p_dev_accum_mean) * 100000)
+        scene["kaiserlich_rot_thresh_x"] = 5 - ((dx_var_mean * 0.0036) * 1000)
+        scene["kaiserlich_rot_thresh_y"] = 5 - ((dy_var_mean * 0.0067) * 1000)
+        scene["kaiserlich_scale_thresh_min"] = 2 - ((rel_var_mean * 0.00024) * 10000)
+        scene["kaiserlich_scale_thresh_max"] = 9 - ((rel_var_mean * 0.00012) * 100000)
+        scene["kaiserlich_rot_scale_thresh_rot"] = 5 - ((((dx_var_mean * 0.0036) + (dy_var_mean * 0.0067)) / 2) * 1000)
+        scene["kaiserlich_rot_scale_thresh_scale"] =  1 - ((rel_var_mean * 0.00018) * 1000)
+        scene["kaiserlich_perspective_thresh"] = 30 - ((global_p_dev_accum_mean * 4) * 100)
 
     except Exception:
         pass
