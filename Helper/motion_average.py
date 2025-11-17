d04f7495748a457322bbc1fc2cf71347095a5783
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-17 00:37:09 +0000

    Passe die Berechnung der Schwellenwerte für die Bewegungsanalyse an: Teile die Mittelwerte durch 100 anstelle von 10.

--- Helper/motion_average.py
+++ Helper/motion_average.py
@@ -194,8 +194,8 @@ def get_from_selected_tracks(context: bpy.types.Context, max_frames: int = 10) -
         dy_var_mean = dy_var_accum / count if count else 0.0
         rel_var_mean = rel_var_accum / count if count else 0.0
 
-        scene["kaiserlich_rot_thresh_x"] = dx_var_mean / 10
-        scene["kaiserlich_rot_thresh_y"] = dy_var_mean / 10
+        scene["kaiserlich_rot_thresh_x"] = dx_var_mean / 100
+        scene["kaiserlich_rot_thresh_y"] = dy_var_mean / 100
         # scene["kaiserlich_scale_thresh_max"] = rel_var_mean
         # scene["kaiserlich_scale_thresh_min"] = rel_var_mean * 0.5
         # scene["kaiserlich_rot_scale_thresh_rot"] = rel_var_mean
