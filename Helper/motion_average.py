8844f5ee4300a640e2a5cff36660537524b80eab
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-17 23:19:08 +0100

    Update motion_average.py

--- Helper/motion_average.py
+++ Helper/motion_average.py
@@ -249,7 +249,7 @@ def get_from_selected_tracks(
         scene["kaiserlich_rot_thresh_y"] = 5 - ((dy_var_mean * 0.0067) * 1000)
         scene["kaiserlich_scale_thresh_min"] = 2 - ((rel_var_mean * 0.00024) * 10000)
         scene["kaiserlich_scale_thresh_max"] = 8 - ((rel_var_mean * 0.00012) * 103500)
-        scene["kaiserlich_rot_scale_thresh_rot"] = 5 - ((((dx_var_mean * 0.0036) + (dy_var_mean * 0.0067)) / 2) * 1200)
+        scene["kaiserlich_rot_scale_thresh_rot"] = 5 - ((((dx_var_mean * 0.0036) + (dy_var_mean * 0.0067)) / 2) * 1150)
         scene["kaiserlich_rot_scale_thresh_scale"] =  6 - ((rel_var_mean * 0.00018) * 50500)
         scene["kaiserlich_perspective_thresh"] = 30 - ((global_p_dev_accum_mean * 4) * 100)
 
