e7fa34fcfbfe6c9a09ab03c6d552dfb080185772
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-17 18:33:52 +0100

    Update motion_average_backwards.py

--- Helper/motion_average_backwards.py
+++ Helper/motion_average_backwards.py
@@ -263,8 +263,8 @@ def get_from_selected_tracks_backwards(
         scene["kaiserlich_scale_thresh_max"] = (rel_var_mean / 1000) * 100000
         scene["kaiserlich_scale_thresh_min"] = (rel_var_mean / 500) * 100000
         scene["kaiserlich_rot_scale_thresh_rot"] = (((dx_var_mean / 250) + (dy_var_mean / 250)) / 2) * 100000
-        scene["kaiserlich_rot_scale_thresh_scale"] =  (rel_var_mean / 750) * 100000
-        scene["kaiserlich_perspective_thresh"] = min(1000, (global_p_dev_accum_mean / 10) * 1000000)
+        scene["kaiserlich_rot_scale_thresh_scale"] =  (rel_var_mean / 1000) * 100000
+        scene["kaiserlich_perspective_thresh"] = min(1000, (global_p_dev_accum_mean / 100) * 1000000)
 
 
     except Exception:
