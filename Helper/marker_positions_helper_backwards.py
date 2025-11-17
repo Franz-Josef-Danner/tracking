f269741096e79fccd3fbff69c1be7ea8989d79fb
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-17 16:50:07 +0100

    Aktualisieren von marker_positions_helper_backwards.py

--- Helper/marker_positions_helper_backwards.py
+++ Helper/marker_positions_helper_backwards.py
@@ -7,7 +7,7 @@ def get_positions_backward(track: 'bpy.types.MovieTrackingTrack', current_frame:
     markers = track.markers
     positions: list[tuple[int, any]] = []
 
-    start_frame = current_frame - (max_frames - 1)
+    start_frame = current_frame - (max_frames + 1)
 
     for frame in range(start_frame, current_frame + 1):
 
