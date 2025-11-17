fdb44dba232fc8544052942bbfa359c7a22f0e46
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-17 22:00:51 +0100

    Adjust max scale threshold calculation

--- Helper/motion_average.py
+++ Helper/motion_average.py
@@ -248,7 +248,7 @@ def get_from_selected_tracks(
         scene["kaiserlich_rot_thresh_x"] = 500 - (((dx_var_mean) * 0.0036) * 10000)
         scene["kaiserlich_rot_thresh_y"] = 800 - (((dy_var_mean) * 0.0067) * 10000)
         scene["kaiserlich_scale_thresh_min"] = 20 - (((rel_var_mean) * 0.00024) * 10000)
-        scene["kaiserlich_scale_thresh_max"] = 10 - (((rel_var_mean) * 0.00012) * 10000)
+        scene["kaiserlich_scale_thresh_max"] = 1 - (((rel_var_mean) * 0.00012) * 10000)
         scene["kaiserlich_rot_scale_thresh_rot"] = 650 - ((((dx_var_mean * 0.0036) + (dy_var_mean * 0.0067)) / 2) * 10000)
         scene["kaiserlich_rot_scale_thresh_scale"] =  20 - (((rel_var_mean) * 0.00018) * 10000)
         scene["kaiserlich_perspective_thresh"] = 1000 - min(1000, (global_p_dev_accum_mean) * 10000)
