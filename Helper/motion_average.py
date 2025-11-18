ae2b84b4d59673270e1497661a75a0b2d9eafc8c
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-18 18:43:58 +0100

    Adjust kaiserlich_rot_thresh_y scaling factor

--- Helper/motion_average.py
+++ Helper/motion_average.py
@@ -246,7 +246,7 @@ def get_from_selected_tracks(
         rel_var_mean = sum(rel_var_accum) / len(rel_var_accum) if rel_var_accum else 0.0
 
         scene["kaiserlich_rot_thresh_x"] = max(0, min(1, 1 - ((dx_var_mean * 0.0036) )))
-        scene["kaiserlich_rot_thresh_y"] = max(0, min(1, 1 - ((dy_var_mean * 0.0067) * 10)))
+        scene["kaiserlich_rot_thresh_y"] = max(0, min(1, 1 - ((dy_var_mean * 0.0067) * 100)))
         scene["kaiserlich_scale_thresh_min"] = max(0, min(1, 1 - ((rel_var_mean * 0.00024) * 4000)))
         scene["kaiserlich_scale_thresh_max"] = max(0, min(1, 1 - ((rel_var_mean * 0.00012) * 5000)))
         scene["kaiserlich_rot_scale_thresh_rot"] = max(0, min(1, 1 - ((((dx_var_mean * 0.0036) + (dy_var_mean * 0.0067)) / 2) * 100)))
