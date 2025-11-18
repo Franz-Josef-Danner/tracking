9f77b8bdbc02d6f1236988e1ce337ec5a4eef1b7
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-18 18:30:13 +0100

    Adjust kaiserlich_scale_thresh_min calculation factor

--- Helper/motion_average.py
+++ Helper/motion_average.py
@@ -247,7 +247,7 @@ def get_from_selected_tracks(
 
         scene["kaiserlich_rot_thresh_x"] = max(0, min(1, 1 - ((dx_var_mean * 0.0036) )))
         scene["kaiserlich_rot_thresh_y"] = max(0, min(1, 1 - ((dy_var_mean * 0.0067) )))
-        scene["kaiserlich_scale_thresh_min"] = max(0, min(1, 1 - ((rel_var_mean * 0.00024) * 50000)))
+        scene["kaiserlich_scale_thresh_min"] = max(0, min(1, 1 - ((rel_var_mean * 0.00024) * 25000)))
         scene["kaiserlich_scale_thresh_max"] = max(0, min(1, 1 - ((rel_var_mean * 0.00012) * 5000)))
         scene["kaiserlich_rot_scale_thresh_rot"] = max(0, min(1, 1 - ((((dx_var_mean * 0.0036) + (dy_var_mean * 0.0067)) / 2) * 100)))
         scene["kaiserlich_rot_scale_thresh_scale"] =  max(0, min(1, 1 - ((rel_var_mean * 0.00018) * 2500)))
