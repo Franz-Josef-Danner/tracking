12740dd9b23b3c31fda541493c35998f81bf0e88
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-19 02:28:49 +0000

    Entferne Debug-Ausgaben aus der Bewegungsmodell-Logik

--- Helper/formula_helper.py
+++ Helper/formula_helper.py
@@ -170,7 +170,6 @@ def apply_formula_on_selected_tracks(context: bpy.types.Context, max_frames: int
         perspective_thresh = getattr((scene, "kaiserlich_perspective_thresh", 0.002)) / 657.0302234
         if global_p_dev > perspective_thresh:
             global_model = "Perspective"
-        print (scene, "kaiserlich_perspective_thresh", perspective_thresh)
         # --- 3) Pro Track anwenden (Priorität: Perspective > LocRotScale > LocScale > LocRot > Loc) ---
         for track in selected_tracks:
             positions = get_positions(track, current_frame, max_frames=max_frames)
@@ -190,10 +189,6 @@ def apply_formula_on_selected_tracks(context: bpy.types.Context, max_frames: int
                     getattr((scene, "kaiserlich_rot_scale_thresh_rot", 0.002) + 0.57) / 376.2227239,
                     getattr((scene, "kaiserlich_rot_scale_thresh_scale", 0.005) + 0.79) / 11111.11111
                 )
-                print (scene, "kaiserlich_rot_thresh_x", getattr(scene, "kaiserlich_rot_thresh_x", 0.002))
-                print (scene, "kaiserlich_scale_thresh_max", getattr(scene, "kaiserlich_scale_thresh_max", 0.005))
-                print (scene, "kaiserlich_rot_scale_thresh_rot", getattr(scene, "kaiserlich_rot_scale_thresh_rot", 0.002))
-                print (scene, "kaiserlich_rot_scale_thresh_scale", getattr(scene, "kaiserlich_rot_scale_thresh_scale", 0.005))
 
                 # Hybrid: wenn Marker stark abweicht, nimm sein Modell, sonst global
                 motion_model = individual_model if individual_model != global_model else global_model
