ed6d5f1c68f5f2652554884a5094d42d6589e101
Franz-Josef-Danner <90143544+Franz-Josef-Danner@users.noreply.github.com>
2025-11-21 02:19:33 +0000

    Ändere Stärke der multiplikativen Normalisierung in get_from_selected_tracks für sanftere Anpassungen der Variablenwerte.

--- Helper/motion_average.py
+++ Helper/motion_average.py
@@ -356,7 +356,7 @@ def get_from_selected_tracks(
 
         # Prozentuale, multiplikative Normalisierung (sanft)
         if max_val > 0:
-            k = 0.5  # Stärke der Normalisierung
+            k = 0.05  # Stärke der Normalisierung
             inv = (1.0 / max_val) ** k
 
             dx_var_mean *= inv
